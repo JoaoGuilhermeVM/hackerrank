d42fed2b5a82f9de67d2bb78397b0ae95c2a1663
JoaoGuilhermeV <jmoitinho@ciandt.com>
2022-08-24 11:21:23 -0300

    [HackerRank][Desafio2] entendimento da dependencia Scanner

--- src/main/java/Solution1.java
+++ src/main/java/Solution1.java
@@ -1,6 +1,6 @@
 package main.java;
 
-public class Solution {
+public class Solution1 {
 
     public static void main(String[] args) {
         /* Enter your code here. Print output to STDOUT. Your class should be named Solution. */
